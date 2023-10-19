58ff112300ad2c9dcb561c90543c08063a1f6a4c
Theresa Maria Breitfuss <mt.breitfuss@protonmail.com>
2023-10-19 10:30:38 +0100

    data structure to represent group of acquaintances

--- group.py
+++ group.py
@@ -1,5 +1,35 @@
-"""An example of how to represent a group of acquaintances in Python."""
+# Define a class for a Person
+class Person:
+    def __init__(self, name, age, job=None):
+        self.name = name
+        self.age = age
+        self.job = job
+        self.connections = {}  # Dictionary to store relationships
 
-# Your code to go here...
+    def add_connection(self, person, relation):
+        """Add a connection to another person."""
+        self.connections[person.name] = {"relation": relation, "person": person}
+        # Assuming the relationship is reciprocal
+        person.connections[self.name] = {"relation": relation, "person": self}
 
-my_group =
+    def __repr__(self):
+        return f"{self.name}, {self.age}, {self.job}, connections: {[person for person in self.connections]}"
+
+# Create instances of the class
+jill = Person("Jill", 26, "biologist")
+zalika = Person("Zalika", 28, "artist")
+john = Person("John", 27, "writer")
+nash = Person("Nash", 34, "chef")
+
+# Add connections
+jill.add_connection(zalika, "friend")
+jill.add_connection(john, "partner")
+john.add_connection(jill, "partner")
+john.add_connection(nash, "cousin")
+nash.add_connection(zalika, "landlord")
+
+# Print the persons
+print(jill)
+print(zalika)
+print(john)
+print(nash)
